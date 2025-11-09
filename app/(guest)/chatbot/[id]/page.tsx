5426403c2c8f535679107add3ad7de094c5d375d
aviralSingh01 <aviral.singh@revspire.net>
2025-11-09 11:01:06 +0530

    open ai integration

--- app/(guest)/chatbot/[id]/page.tsx
+++ app/(guest)/chatbot/[id]/page.tsx
@@ -9,16 +9,32 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { GetChatbotByIdResponse, GetChatbotByIdVariables, Message, MessagesByChatSessionIdResponse, MessagesByChatSessionIdVariables } from "@/types/types";
+import {
+  GetChatbotByIdResponse,
+  GetChatbotByIdVariables,
+  Message,
+  MessagesByChatSessionIdResponse,
+  MessagesByChatSessionIdVariables,
+} from "@/types/types";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import startNewChat from "@/lib/server/startNewChat";
 import Avatar from "@/components/Avatar";
 import { useQuery } from "@apollo/client";
-import { GET_CHATBOT_BY_ID, GET_MESSAGES_BY_CHAT_SESSION_ID } from "@/graphql/queries/queries";
-import { Variable } from "lucide-react";
+import {
+  GET_CHATBOT_BY_ID,
+  GET_MESSAGES_BY_CHAT_SESSION_ID,
+} from "@/graphql/queries/queries";
 import Messages from "@/components/Messages";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+
+const formSchema = z.object({
+    message: z.string().min(2,"Your message is too short!"),
+})
 
 const ChatbotPage = ({ params: { id } }: { params: { id: string } }) => {
   const [name, setName] = useState("");
@@ -28,58 +44,136 @@ const ChatbotPage = ({ params: { id } }: { params: { id: string } }) => {
   const [loading, setLoading] = useState(false);
   const [messages, setMessages] = useState<Message[]>([]);
 
-
-//   get the chatbot data
-  const {data:chatbotData} = useQuery<GetChatbotByIdResponse,GetChatbotByIdVariables>(
-    GET_CHATBOT_BY_ID,{
-        variables:{id}
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver:zodResolver(formSchema),
+    defaultValues:{
+        message:"",
     }
-  )
+  })
 
-//   get the messages by chatbot_sessiosn_id
+  //   get the chatbot data
+  const { data: chatbotData } = useQuery<
+    GetChatbotByIdResponse,
+    GetChatbotByIdVariables
+  >(GET_CHATBOT_BY_ID, {
+    variables: { id },
+  });
 
-const {
-    loading:loadingQuery,
+  //   get the messages by chatbot_sessiosn_id
+
+  const {
+    loading: loadingQuery,
     error,
     data,
-} = useQuery<MessagesByChatSessionIdResponse,MessagesByChatSessionIdVariables>(
-    GET_MESSAGES_BY_CHAT_SESSION_ID,{
-        variables:{
-            chat_session_id:chatId
-        },
-        skip:!chatId,
+  } = useQuery<
+    MessagesByChatSessionIdResponse,
+    MessagesByChatSessionIdVariables
+  >(GET_MESSAGES_BY_CHAT_SESSION_ID, {
+    variables: {
+      chat_session_id: chatId,
+    },
+    skip: !chatId,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setMessages(data.chat_sessions.messages);
     }
-)
+  }, [data]);
 
-
-useEffect(()=>{
-    if(data){
-        setMessages(data.chat_sessions.messages)
-    }
-
-
-},[data])
-
-
-
-  const handleInformatoinSubmit = async(e:React.FormEvent) =>{
+  const handleInformatoinSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
     // console.log(1);
-    const chatId = await startNewChat(name,email,Number(id));
+    const chatId = await startNewChat(name, email, Number(id));
     // console.log("2")
     setChatId(chatId);
     setLoading(false);
     setIsOpen(false);
-  }
+  };
+
+//   onsubmit function for the messages
+async function onSubmit( values : z.infer<typeof formSchema>){
+    setLoading(true);
+    const {message : formMessage} = values;
+    const message = formMessage;
+    form.reset();
+
+    if(!name||!email){
+        // fill the details in the modal
+        setIsOpen(true);
+        setLoading(false);
+        return;
+    }
+
+    if(!message.trim()){
+        return; //do not submit if the message is empty
+    }
+
+    // optmistically update the ui with the user's message
+
+    const  userMessage:Message = {
+        id:Date.now(),
+        content:message,
+        created_at:new Date().toISOString(),
+        chat_session_id:chatId,
+        sender:"user"
+    }
 
 
+    // immediately fake a message - thinking...
+    
+    const loadingMessage :Message ={
+        id:Date.now()+1,
+        content:"Thinking...",
+        created_at:new Date().toISOString(),
+        chat_session_id:chatId,
+        sender:"ai"
+    }
+
+    setMessages((prevMessages)=>[
+        ...prevMessages,
+        userMessage,
+        loadingMessage,
+    ])
+
+    // make the api call
+
+    try {
+
+        const response = await fetch("/api/send-message",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify({
+                name:name,
+                chat_session_id:chatId,
+                chatbot_id:id,
+                content:message,
+            }),
+        });
+
+        const result =  await response.json();
+
+        // update the ui
+        // update thepreviosuly loading message with the response from AI
+        setMessages((prevMessages)=>
+        prevMessages.map((msg)=>
+        msg.id === loadingMessage.id ? {...msg,content:result.content,id:result.id}:msg))
+
+    } catch (error) {
+        console.error("Error in sendin the message:",error)
+        
+    }
+
+}
 
   return (
     <div className="w-full flex bg-gray-100">
       <Dialog open={isOpen} onOpenChange={setIsOpen}>
         <DialogContent className="w-[350px] sm:min-w-[425px]">
-          <form  onSubmit={handleInformatoinSubmit}>
+          <form onSubmit={handleInformatoinSubmit}>
             <DialogHeader className="font-bold w-full items-center justify-center">
               Lets help you out!
             </DialogHeader>
@@ -117,38 +211,69 @@ useEffect(()=>{
             </div>
 
             <DialogFooter>
-                <Button type="submit" disabled={!name||!email||loading}>
-                    {!loading?"Continue":"Loading..."}
-                </Button>
+              <Button type="submit" disabled={!name || !email || loading}>
+                {!loading ? "Continue" : "Loading..."}
+              </Button>
             </DialogFooter>
           </form>
-
         </DialogContent>
       </Dialog>
 
       {/* the chatbot ui here  */}
 
-      <div className="flex flex-col w-full max-w-3xl mx-auto bg-white md:rounded-l-lg shadow-2xl md:mt-10">
+      <div className="flex flex-col min-h-screen w-full max-w-3xl mx-auto bg-white md:rounded-l-lg shadow-2xl md:mt-10">
         <div className="pb-4 border-b sticky top-0 z-50 bg-[#4D7DFB] py-5 px-10 text-white md:rounded-t-lg flex  items-center space-x-4">
-            <Avatar
+          <Avatar
             seed={chatbotData?.chatbots.name as string}
-            className="h-12 w-12 bg-white rounded-full border-2 border-white"/>
+            className="h-12 w-12 bg-white rounded-full border-2 border-white"
+          />
 
-            <div>
-                <h1 className="truncate text-lg">{chatbotData?.chatbots.name}</h1>
-                <p className="text-sm text-gray-300">
-                    Typically Replies Instantly
-                </p>
-            </div>
+          <div>
+            <h1 className="truncate text-lg">{chatbotData?.chatbots.name}</h1>
+            <p className="text-sm text-gray-300">Typically Replies Instantly</p>
+          </div>
         </div>
-
 
         {/* All the messages  */}
 
         <Messages
-            messages={messages}
-            chatbotName={chatbotData?.chatbots.name!}
-            />
+          messages={messages}
+          chatbotName={chatbotData?.chatbots.name!}
+        />
+
+      
+        <Form {...form}>
+            <form
+            onSubmit={form.handleSubmit(onSubmit)} 
+            className="flex items-start sticky bottom-0 z-50 space-x-4 p-5 h-[5.5rem] bg-gray-100 rounded-md">
+                <FormField
+                  control={form.control}
+                  name="message"
+                  render={({field}) => (
+                    <FormItem className="flex-1">
+                        <FormLabel hidden>Message</FormLabel>
+                        <FormControl>
+
+                            <Input 
+                            placeholder="Type a message..."
+                            {...field}
+                            className="p-5"
+                            />
+
+
+                        </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                <Button 
+                type="submit"
+                disabled={form.formState.isSubmitting || !form.formState.isValid}
+                className="h-full flex w-24 items-center justify-center">Send</Button>
+            </form>
+
+        </Form>
+
 
 
       </div>
---
+++ app/api/send-message/route.ts
@@ -0,0 +1,135 @@
+import { INSERT_MESSAGE } from "@/graphql/mutations/mutations";
+import { GET_CHATBOT_BY_ID, GET_MESSAGES_BY_CHAT_SESSION_ID } from "@/graphql/queries/queries";
+import { serverClient } from "@/lib/server/serverClient";
+import { GetChatbotByIdResponse, MessagesByChatSessionIdResponse } from "@/types/types";
+import { NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+
+const openai = new OpenAI({
+    apiKey:process.env.OPENAI_API_KEY,
+})
+
+export async function POST(req:NextRequest) {
+    const {chat_session_id , chatbot_id, content, name } = await req.json();
+
+    console.log(`Revieced message from
+         chat session ${chat_session_id} : ${content} (chatbot : ${chatbot_id}) 
+         by (user ${name}) `);
+
+    try {
+        // step 1 : fethc the chatbot characteristics
+        const {data } = await serverClient.query<GetChatbotByIdResponse>({
+            query:GET_CHATBOT_BY_ID,
+            variables:{
+                id:chatbot_id
+            }
+        })
+        const chatbot = data.chatbots;
+
+        if(!chatbot){
+            return NextResponse.json({error:"Chatbot not found"},{status:404});
+        }
+
+        //2. fetch the users previous messages from the chat 
+        const {data:messagesData} = await serverClient.query<MessagesByChatSessionIdResponse>({
+            query:GET_MESSAGES_BY_CHAT_SESSION_ID,
+            variables:{
+                chat_session_id
+            },
+            fetchPolicy:"no-cache"
+        })
+
+        const prevousMessages =  messagesData.chat_sessions.messages;
+
+        // 3. put the data in coreect format for the ai service (openai or gemini)
+
+        const formattedPreviousMessages:ChatCompletionMessageParam[] =
+        prevousMessages.map((message)=>(
+            {
+                role:message.sender === "ai"?"system":"user",
+                name:message.sender === "ai"?"system":name,
+                content:message.content,
+            }
+        ))
+
+
+        // 4. combines the system characteristics into a single prompt
+        const systemPrompt = chatbot.chatbot_characteristics.map((c)=>c.content).join(" + ");
+
+        console.log(systemPrompt);
+
+        const messages:ChatCompletionMessageParam[] =[
+            {
+                role:"system",
+                name:"system",
+                content:`You are a helpful assistant talkign to ${name}.
+                If a generic question is asked which is not relevant or in the same scope or domainas the points
+                in the mentioned key information section, kindly inform the user they are only allowed to search for the specified content
+                . Use emoji's where possible or required. 
+                Here is the kay information that you need to be aware of, these are elements you may be asked about:
+                ${systemPrompt}`
+            },
+            ...formattedPreviousMessages,
+            {
+                role:"user",
+                name:name,
+                content:content
+            },
+        ]
+
+
+        // 5 get the response 
+        const response = await openai.chat.completions.create({
+            messages:messages,
+            model:"gpt-3.5-turbo"
+        })
+
+        const aiResponse =  response?.choices?.[0]?.message?.content?.trim();
+
+        if(!aiResponse){
+            return NextResponse.json(
+                {
+                    error:"Faield to generate ai prompt"
+                },{
+                    status:500
+                }
+            )
+        }
+
+        // 6. push into the database
+
+        await serverClient.mutate({
+            mutation:INSERT_MESSAGE,
+            variables:{
+                chat_session_id,
+                content,
+                sender:"user"
+            }
+        })
+
+        // 7. save the ai response into the database  
+
+       const aiMessageResult =  await serverClient.mutate({
+            mutation:INSERT_MESSAGE,
+            variables:{chat_session_id,content:aiResponse,sender:"ai"}
+        })
+
+        // 8 return the ai repsonse to the user 
+        return NextResponse.json({
+            id:aiMessageResult.data.insertMessages.id,
+            content:aiResponse,
+        })
+
+
+
+
+        
+    } catch (error) {
+        console.error("Error in /send-message ",error);
+        return NextResponse.json({error},{status:500}); 
+    }  
+
+
+    
+}
